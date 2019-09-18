7ccb943c1b55cd0145683be23e679fa6a72c5ad2
Damyan Peykov <dppeykov@gmail.com>
2019-09-18 11:06:43 +0200

    Updating with f-strings

--- notes.py
+++ notes.py
@@ -4,3 +4,19 @@
 # VARIABLES
 # Naming - can contain only letters, numbers, and underscores; can't start with a number; no spacing - use _; short & descriptive; don't use keywords 
 # Just a label to a box (value) in the memory - the variable is referencing the value
+
+message = "Hello World"
+print(message)
+
+# STRINGS
+string_descr = "could be with ' or \" or \"\"\" for docstrings"
+print("clear:", string_descr)
+print("title() - capitalizing every word's first letter:", string_descr.title())
+print("upper() - capitalizing all the words:", string_descr.upper())
+print("lower() - lowercase:", string_descr.lower())
+
+# f-strings - python 3.6+
+first_name = "ada"
+last_name = "lovelace"
+print(f"F-string: Hello, {first_name.title()} {last_name.title()}!")
+print("Format method: Hello, {} {}!".format(first_name.title(), last_name.title()))
