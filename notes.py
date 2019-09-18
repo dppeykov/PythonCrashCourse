17c689ddf97c3b28bee29de4f5ccdd6e0cf84812
Damyan Peykov <dppeykov@gmail.com>
2019-09-18 10:29:16 +0200

    Creating notes as I go through the book

---
+++ notes.py
@@ -0,0 +1,6 @@
+# HOW PYTHON WORKS: Python source code -->> compile step -->> bytecode -->> interpreted by the python virtual machine (virtual CPU) 
+# Note: The VM is written in C and when running a python program it gets compiled to machine code, so then it can interpreter and run the bytecode
+
+# VARIABLES
+# Naming - can contain only letters, numbers, and underscores; can't start with a number; no spacing - use _; short & descriptive; don't use keywords 
+# Just a label to a box (value) in the memory - the variable is referencing the value
